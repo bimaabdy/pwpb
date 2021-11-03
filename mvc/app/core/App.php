b520a4bd2dc7512e3c7a3c13d7ed89d50a0bb80a
bimaabdy <93626287+bimaabdy@users.noreply.github.com>
2021-11-03 08:53:09 +0800

    Add files via upload

---
+++ mvc/app/core/App.php
@@ -0,0 +1,19 @@
+<?php
+
+	class App
+	{
+
+		public function parseURL() {
+	  		if(isset($_GET["url"])) {
+	  			$url = rtrim($_GET["url"],'/');
+	  			$url = filter_var($url, FILTER_SANITIZE_URL);
+	  			$url = explode('/', $url);
+	  			return $url;
+		}
+	
+	}
+	public function __construct() {
+		echo "Web MVC Siap Dijalankan!";
+	}
+
+}
